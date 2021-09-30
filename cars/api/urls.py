846dc6b72cfd613e830360b487a608f346766623
Mathieu Robichaud <admin@birkoss.com>
2021-09-30 06:34:51 -0400

    Changed URL endpoint

--- cars/api/urls.py
+++ cars/api/urls.py
@@ -5,12 +5,12 @@ from . import views as api_views
 
 urlpatterns = [
     path(
-        'api/dealer/<str:dealer_id>/cars',
+        'dealer/<str:dealer_id>/cars',
         api_views.cars.as_view(),
         name='cars'
     ),
     path(
-        'api/car/<str:car_id>',
+        'car/<str:car_id>',
         api_views.car.as_view(),
         name='car'
     ),
--- dealers/api/urls.py
+++ dealers/api/urls.py
@@ -5,12 +5,12 @@ from . import views as api_views
 
 urlpatterns = [
     path(
-        'api/dealers',
+        'dealers',
         api_views.dealers.as_view(),
         name='dealers'
     ),
     path(
-        'api/dealer/<str:dealer_id>',
+        'dealer/<str:dealer_id>',
         api_views.dealer.as_view(),
         name='dealer'
     ),
