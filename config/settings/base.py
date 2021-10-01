614a1c9ccfdda362fc91213a0c35a2b8968dbd91
Mathieu Robichaud <admin@birkoss.com>
2021-10-01 11:48:35 -0400

    Added cors

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,6 +8,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
     'birkoss',
     'users',
@@ -18,6 +19,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
